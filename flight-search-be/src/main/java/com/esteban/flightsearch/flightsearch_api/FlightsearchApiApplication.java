99fd1a29dd1a3491dc1f5794ebf594849eac99cf
estebantapia-encora <esteban.tapia@encora.com>
2025-05-06 05:17:02 -0700

    Add full backend and frontend to main repository

---
+++ flight-search-be/src/main/java/com/esteban/flightsearch/flightsearch_api/FlightsearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.esteban.flightsearch.flightsearch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsearchApiApplication.class, args);
+	}
+
+}
---
+++ flight-search-be/src/test/java/com/esteban/flightsearch/flightsearch_api/FlightsearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esteban.flightsearch.flightsearch_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
