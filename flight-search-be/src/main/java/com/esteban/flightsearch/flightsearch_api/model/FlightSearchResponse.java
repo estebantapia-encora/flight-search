1f0678f9adf5d2cac124518f861ff4f852758af5
estebantapia-encora <esteban.tapia@encora.com>
2025-05-10 19:28:37 -0700

    Added currency to request

--- flight-search-be/src/main/java/com/esteban/flightsearch/flightsearch_api/model/FlightSearchResponse.java
+++ flight-search-be/src/main/java/com/esteban/flightsearch/flightsearch_api/model/FlightSearchResponse.java
@@ -8,14 +8,9 @@ public class FlightSearchResponse {
     private String arrival;
     private String airline;
     private String price;
-<<<<<<< Updated upstream
     private String departureTime;
     private String arrivalTime;
     private String duration;
-=======
     private String currency;
-    private String departureTime;
-    private String arrivalTime;
->>>>>>> Stashed changes
     private int numberOfStops;
 }
