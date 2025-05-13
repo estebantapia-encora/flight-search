c19d22bff5d527f91fb23cc2a1847e0ec336a9e6
estebantapia-encora <esteban.tapia@encora.com>
2025-05-13 14:28:27 -0700

    Added mock results to test post request response

--- flight-search-fe/src/components/SearchButton.tsx
+++ flight-search-fe/src/components/SearchButton.tsx
@@ -1,19 +1,22 @@
 import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 
 type SearchButtonProps = {
   onClick: () => void;
+  loading: boolean;
 };
 
-export default function SearchButton({onClick}:SearchButtonProps) {
-
+export default function SearchButton({ onClick, loading }: SearchButtonProps) {
   return (
     <>
-      <Button 
-      variant="contained" 
-      size="large" 
-      onClick={onClick}
-      sx={{ width: "15%" }}>
-        Search
+      <Button
+        variant="contained"
+        size="large"
+        onClick={onClick}
+        disabled={loading}
+        sx={{ width: "15%", position: "relative" }}
+      >
+        {loading ? <CircularProgress size={24} color="inherit" /> : "Search"}
       </Button>
     </>
   );
