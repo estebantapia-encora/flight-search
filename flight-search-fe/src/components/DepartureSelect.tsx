39bf532211d979d2843b0182eb649d21ceab7945
estebantapia-encora <esteban.tapia@encora.com>
2025-05-13 11:33:09 -0700

    Autocomplete added to departure and arrival airport

--- flight-search-fe/src/components/DepartureSelect.tsx
+++ flight-search-fe/src/components/DepartureSelect.tsx
@@ -2,43 +2,28 @@ import { useEffect, useState } from "react";
 import { Box, Typography, Autocomplete, TextField } from "@mui/material";
 
 type DepartureSelectProps = {
-  value: string; // controlled input from parent
   onChange: (value: string) => void; // gets called with IATA code
 };
 
 export default function DepartureSelect({
-  value,
   onChange,
 }: DepartureSelectProps) {
+  
   const [options, setOptions] = useState<string[]>([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
-    if (value.length < 2) return;
+    if (searchText.length < 2) return;
 
     const fetchAirports = async () => {
-      try {
-        const response = await fetch(
-          `/api/airports/search?keyword=${value.toUpperCase()}`
-        );
-        const data = await response.json();
-
-        console.log("Fetched airport data:", data); // Debug
-
-        if (Array.isArray(data)) {
-          setOptions(data);
-        } else {
-          console.error("Expected array but got:", data);
-          setOptions([]);
-        }
-      } catch (err) {
-        console.error("Failed to fetch airports", err);
-        setOptions([]);
-      }
+      const response = await fetch(`/api/airports/search?keyword=${searchText.toUpperCase()}`);
+      const data = await response.json();
+      setOptions(Array.isArray(data) ? data : []);
     };
-    const delayDebounce = setTimeout(fetchAirports, 200);
-
+  
+    const delayDebounce = setTimeout(fetchAirports, 300);
     return () => clearTimeout(delayDebounce);
-  }, [value]);
+  }, [searchText]);
 
   return (
     <Box sx={{ width: "100%", display: "flex", alignItems: "center" }}>
@@ -49,13 +34,13 @@ export default function DepartureSelect({
         sx={{ m: 1, width: 250 }}
         size="small"
         options={options}
-        inputValue={value}
-        onInputChange={(_, newInput) => onChange(newInput)}
-        onChange={(_, newValue) => {
-          const match = /\(([^)]+)\)/.exec(newValue || "");
-          const iataCode = match ? match[1] : "";
-          onChange(iataCode);
-        }}
+        inputValue={searchText}
+  onInputChange={(_, newInput) => setSearchText(newInput)}
+  onChange={(_, newValue) => {
+    const match = /\(([^)]+)\)/.exec(newValue || "");
+    const iataCode = match ? match[1] : "";
+    onChange(iataCode);
+  }}
         renderInput={(params) => (
           <TextField {...params} label="Where From?" variant="outlined" />
         )}
