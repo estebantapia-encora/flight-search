8c2c05d491673e607d2c171820b9db73f4cea759
estebantapia-encora <esteban.tapia@encora.com>
2025-05-17 15:46:42 -0700

    Airline code to airline common name can be visualized in results and details page

--- flight-search-be/src/main/java/com/esteban/flightsearch/flightsearch_api/model/AirlineLookupResponse.java
+++ flight-search-be/src/main/java/com/esteban/flightsearch/flightsearch_api/model/AirlineLookupResponse.java
@@ -1,4 +1,27 @@
 package com.esteban.flightsearch.flightsearch_api.model;
 
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
 public class AirlineLookupResponse {
+    private Meta meta;
+    private List<AirlineData> data;
+
+    @Data
+    public static class Meta {
+        private int count;
+        private Map<String, String> links; // Puedes usar Map si no te interesa modelar "self"
+    }
+
+    @Data
+    public static class AirlineData {
+        private String type; // lo que tú querías incluir
+        private String iataCode;
+        private String icaoCode;
+        private String businessName;
+        private String commonName;
+    }
 }
